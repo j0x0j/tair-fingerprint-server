fb567f0ab68a95181248bc2eaf821277c8cc3dbe
Joel Martinez <jmgalanes@gmail.com>
2017-11-18 22:46:33 -0400

    Initial commit, simple spawner server

---
+++ index.js
@@ -0,0 +1,46 @@
+const restify = require('restify')
+const fs = require('fs')
+const { exec } = require('child_process')
+
+const DEJAVU_APP_PATH = __dirname + '/../dejavu'
+
+function respond (req, res, next) {
+  const SAMPLE_PATH = __dirname + '/samples/' + req.files.file.name
+
+  fs.createReadStream(req.files.file.path)
+    .pipe(fs.createWriteStream(SAMPLE_PATH))
+    .on('finish', () => {
+      const cmd = [
+        'python2.7',
+        `${DEJAVU_APP_PATH}/dejavu.py`,
+        '--config',
+        `${DEJAVU_APP_PATH}/dejavu.cnf.SAMPLE`,
+        '--recognize',
+        'file',
+        SAMPLE_PATH,
+      ]
+
+      exec(cmd.join(' '), (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`)
+          res.json({ status: 'error' })
+          return;
+        }
+        res.json(JSON.parse(stdout))
+        next()
+      })
+    })
+}
+
+const server = restify.createServer()
+
+server.use(restify.plugins.bodyParser({
+  mapFiles: true
+}))
+
+server.post('/sample', respond)
+server.head('/sample', respond)
+
+server.listen(8080, () => {
+  console.log('%s listening at %s', server.name, server.url)
+})
