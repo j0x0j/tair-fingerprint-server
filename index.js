daae47160f51c4d072e685be158f43e8b5dcb20a
Joel Martinez <jmgalanes@gmail.com>
2018-01-21 02:11:32 -0500

    Adds MySQL and remove fingerprint handler

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const restify = require('restify')
 const fs = require('fs')
+const mysql = require('mysql')
 const { exec } = require('child_process')
+const corsMiddleware = require('restify-cors-middleware')
 
 const DEJAVU_APP_PATH = __dirname + '/../dejavu'
+
+// MySQL config
+
+const dbClient = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'dejavu',
+})
 
 function handleSample (req, res, next) {
   const SAMPLE_PATH = __dirname + '/samples/' + req.files.file.name
@@ -68,11 +79,30 @@ function handleFingerprint (req, res, next) {
     })
 }
 
+function handleRemoveFingerprint (req, res, next) {
+  const query = `DELETE from songs WHERE song_name="${req.params.name}"`
+  dbClient.query(query, (error, results, fields) => {
+    if (error) {
+      return res.json(500, { status: 'error', message: error.message })
+    }
+    res.json({ status: 'OK' })
+  })
+}
+
 const server = restify.createServer()
 
 server.use(restify.plugins.bodyParser({
   mapFiles: true
 }))
+
+const cors = corsMiddleware({
+  origins: ['*'],
+  allowHeaders: ['Access-Control-Allow-Origin'],
+  exposeHeaders: ['Access-Control-Allow-Origin']
+})
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 server.post('/sample', handleSample)
 server.head('/sample', handleSample)
@@ -83,6 +113,11 @@ server.head('/health', handleHealth)
 server.post('/fingerprint', handleFingerprint)
 server.head('/fingerprint', handleFingerprint)
 
+server.del('/fingerprint/:name', handleRemoveFingerprint)
+server.head('/fingerprint/:name', handleRemoveFingerprint)
+
 server.listen(process.env.PORT || 8080, () => {
+  // Connect to dejavu DB
+  dbClient.connect()
   console.log('%s listening at %s', server.name, server.url)
 })
