ee5963dfbea5ac8e95698084d0f169d1eba2c783
Joel Martinez <jmgalanes@gmail.com>
2018-06-26 21:41:23 -0400

    Unlinking tmp files when uploaded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql')
 const { exec } = require('child_process')
 const corsMiddleware = require('restify-cors-middleware')
 
-const DEJAVU_APP_PATH = __dirname + '/../dejavu'
+const DEJAVU_APP_PATH = __dirname + '/../dejavu2'
 
 // MySQL config
 
@@ -17,12 +17,13 @@ const dbClient = mysql.createConnection({
 
 function handleSample (req, res, next) {
   const SAMPLE_PATH = __dirname + '/samples/' + req.files.file.name
+  const TEMP_PATH = req.files.file.path
 
-  fs.createReadStream(req.files.file.path)
+  fs.createReadStream(TEMP_PATH)
     .pipe(fs.createWriteStream(SAMPLE_PATH))
     .on('finish', () => {
       const cmd = [
-        'python2.7',
+        'python3',
         `${DEJAVU_APP_PATH}/dejavu.py`,
         '--config',
         `${DEJAVU_APP_PATH}/dejavu.cnf`,
@@ -32,22 +33,22 @@ function handleSample (req, res, next) {
       ]
 
       exec(cmd.join(' '), (error, stdout, stderr) => {
-        fs.unlink(SAMPLE_PATH, (err) => {
-          if (error) {
-            console.error(`exec error: ${error}`)
-            res.json(500, { status: 'error' })
-          } else {
-            res.json(JSON.parse(stdout))
-          }
-          next(err)
-        })
+        fs.unlinkSync(SAMPLE_PATH)
+        fs.unlinkSync(TEMP_PATH)
+        if (error) {
+          console.error(`exec error: ${error}`)
+          res.json(500, { status: 'error' })
+          next(error)
+        } else {
+          res.json(JSON.parse(stdout))
+        }
       })
     })
     .on('error', (error) => {
       console.error(`file error: ${error}`)
-      fs.unlink(SAMPLE_PATH, (err) => {
-        next(err)
-      })
+      fs.unlinkSync(SAMPLE_PATH)
+      fs.unlinkSync(TEMP_PATH)
+      next(error)
     })
 }
 
@@ -60,12 +61,13 @@ function handleHealth (req, res, next) {
 function handleFingerprint (req, res, next) {
   const CREATIVE_DIR = `${DEJAVU_APP_PATH}/wav`
   const CREATIVE_PATH = `${CREATIVE_DIR}/${req.files.file.name}`
+  const TEMP_PATH = req.files.file.path
 
-  fs.createReadStream(req.files.file.path)
+  fs.createReadStream(TEMP_PATH)
     .pipe(fs.createWriteStream(CREATIVE_PATH))
     .on('finish', () => {
       const cmd = [
-        'python2.7',
+        'python3',
         `${DEJAVU_APP_PATH}/dejavu.py`,
         '--config',
         `${DEJAVU_APP_PATH}/dejavu.cnf`,
@@ -81,6 +83,7 @@ function handleFingerprint (req, res, next) {
           res.json(JSON.parse(stdout))
         }
         next(error)
+        fs.unlinkSync(TEMP_PATH)
       })
     })
 }
